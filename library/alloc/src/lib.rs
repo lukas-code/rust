f78dd699a4486d2805727e336f33610be7f5cdeb
Lukas Markeffsky <@>
2022-12-15 09:52:32 +0100

    update bootstrap cfg
    Co-Authored-By: Pietro Albini <pietro.albini@ferrous-systems.com>

--- library/alloc/src/lib.rs
+++ library/alloc/src/lib.rs
@@ -152,7 +152,7 @@
 #![feature(trusted_len)]
 #![feature(trusted_random_access)]
 #![feature(try_trait_v2)]
-#![cfg_attr(not(bootstrap), feature(tuple_trait))]
+#![feature(tuple_trait)]
 #![feature(unchecked_math)]
 #![feature(unicode_internals)]
 #![feature(unsize)]
